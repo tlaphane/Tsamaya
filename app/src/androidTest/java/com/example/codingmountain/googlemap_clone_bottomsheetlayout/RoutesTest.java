63a180786f464f233c26a8dca92bf148ec3e251b
SisDee <1457154@students.wits.ac.za>
2019-10-14 23:04:26 +0200

    modify

--- app/src/androidTest/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/RoutesTest.java
+++ app/src/androidTest/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/RoutesTest.java
@@ -1,19 +1,44 @@
 package com.example.codingmountain.googlemap_clone_bottomsheetlayout;
 
+import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import android.widget.Toast;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 public class RoutesTest {
+ @Rule
+    public ActivityTestRule<Routes>  RoutesTestRule= new ActivityTestRule<Routes>(Routes.class);
+    private Routes RoutesTest =null;
+
+    //Instrumentation.ActivityMonitor monitor= getInstrumentation()
+
+ @Before
+ public void setUp() throws Exception {
+     RoutesTest = RoutesTestRule.getActivity();
+    }
+ @Test
+ public void testLaunchofSecondActivityOnButtonClick(){
+    assertNotNull(RoutesTest.findViewById(R.id.button));
+    onView(withId(R.id.button)).perform(click());
 
 
-
-
+ }
+ @After
+public void tearDown() throws Exception{
+     RoutesTest=null;
+ }
 
 }
