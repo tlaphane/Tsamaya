97a7204c30ac7955dac002f97f085653edf8eecb
SisDee <1457154@students.wits.ac.za>
2019-10-14 21:44:17 +0200

    Merge branch 'master' of /home/dineo/Videos/Tsamaya with conflicts.

---
+++ app/src/androidTest/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/RoutesTest.java
@@ -0,0 +1,19 @@
+package com.example.codingmountain.googlemap_clone_bottomsheetlayout;
+
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import org.junit.Rule;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
+
+public class RoutesTest {
+
+
+
+
+
+}
