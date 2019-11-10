b201d3fa09a75af7b01e20e89e23345d0dbf18a6
Motaung08 <1431795@students.wits.ac.za>
2019-11-10 22:25:20 +0200

    Spinner button for different graphs

--- app/src/main/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/Graphs.java
+++ app/src/main/java/com/example/codingmountain/googlemap_clone_bottomsheetlayout/Graphs.java
@@ -1,33 +1,49 @@
 package com.example.codingmountain.googlemap_clone_bottomsheetlayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.IMarker;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
-public class Graphs extends AppCompatActivity {
+import static com.example.codingmountain.googlemap_clone_bottomsheetlayout.R.*;
 
+public class Graphs extends AppCompatActivity implements GraphsSelectedButton {
 
      BarChart barChart;
 
@@ -35,8 +51,22 @@ public class Graphs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.graphs);
-        barChart =(BarChart) findViewById(R.id.barChart);
+        setContentView(layout.graphs);
+        barChart =(BarChart) findViewById(id.barChart);
+
+//        Spinner spinner = findViewById(id.times);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, array.names, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        String [] timeSchedule = new String[]{"3-5am","6-9am","10-12pm","13-16pm","17-19pm","19-21pm","21-23pm"};
+        ArrayList<String>test=new ArrayList<>(Arrays.asList(timeSchedule));
+
+        Spinner spinner = (Spinner) findViewById(id.times);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, array.times, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         BarDataSet barDataSet1 = new BarDataSet(barEntries1(),"Less than 30mins");
         barDataSet1.setColors(Color.RED);
@@ -45,10 +75,14 @@ public class Graphs extends AppCompatActivity {
         BarDataSet barDataSet3 = new BarDataSet(barEntries3(),"Above an hour");
         barDataSet3.setColors(Color.MAGENTA);
 
+        ArrayList<BarEntry>yValues = new ArrayList<>();
+
         BarData data = new BarData(barDataSet1,barDataSet2,barDataSet3);
         barChart.setData(data);
 
         String[] days= new String[]{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
+
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
         xAxis.setCenterAxisLabels(true);
@@ -58,10 +92,6 @@ public class Graphs extends AppCompatActivity {
         barChart.setDragEnabled(true);
         barChart.setVisibleXRangeMaximum(3);
 
-
-        String [] timeSchedule = new String[]{"3-5am","6-9am","10-12pm","13-16pm","17-19pm","19-21pm","21-23pm"};
-
-
         float barSpace=0.08f;
         float groupSpace=0.44f;
 
@@ -69,13 +99,31 @@ public class Graphs extends AppCompatActivity {
 
         barChart.getXAxis().setAxisMinimum(0);
         barChart.getXAxis().setAxisMaximum(0+barChart.getBarData().getGroupWidth(groupSpace,barSpace)*7);
-
         barChart.getAxisLeft().setAxisMinimum(0);
         barChart.groupBars(0,groupSpace,barSpace);
-
         barChart.invalidate();
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?>parent, View view, int position){
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+
 
     private ArrayList<BarEntry>barEntries1(){
         ArrayList<BarEntry>barEntries = new ArrayList<>();
@@ -118,4 +166,6 @@ public class Graphs extends AppCompatActivity {
 
         return barEntries;
     }
+
+
 }
